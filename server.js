a2ba665187bc3612894a01b26f2efa37d4edc104
TaronKaragyan <taron.karagyan.y@tumo.org>
2024-01-22 21:29:35 +0400

    Idk.. express and mongoose... it's hard

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const port = 3000;
 
 const mongoose = require('mongoose');
 
-// Replace the connection string with your MongoDB connection string
-const connectionString = 'mongodb+srv://TaronKaragyan:094690398Taron@cluster0.jctwsr0.mongodb.net/?retryWrites=true&w=majority';
+const connectionString = 'mongodb+srv://TaronKaragyan:094690398Taron@cluster0.jctwsr0.mongodb.net/sample_mflix';
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
 
 // Define a Mongoose schema and model for the Person collection
 const personSchema = new mongoose.Schema({
@@ -15,24 +17,54 @@ const personSchema = new mongoose.Schema({
     age: Number,
 });
 
-mongoose.connect(connectionString);
 
 const Person = mongoose.model('Person', personSchema);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.post('/submit', (req, res) => {
-    const Name = req.body.name;
-    const Age = req.body.age;
-    console.log('Received data:', Name, Age);
-    data = [{
-        name: Name,
-        age: Age,
-    }]
-    Person.insertMany([data])
-    console.log(data)
-    res.status(200).send('Data received successfully');
-});
+
+// data = [{
+//     name: "Taron1",
+//     email: 'taron.who.tf.caewwfres@gmail.com',
+//     password: '094690398'
+// }]
+// db.once('open', async () => {
+//     try {
+//         const allMovies = await mongoose.connection.db.collection('users')
+
+//         app.post('/submit', (req, res) => {
+//             let Name = req.body.name;
+//             let Email = req.body.email;
+//             let Password = req.body.password;
+
+//             data = [{
+//                 name: Name,
+//                 email: Email,
+//                 passwprd: Password,
+//             }]
+//             allMovies.insertMany(data)
+//             console.log(data)
+//             res.status(200).send('Data received successfully');
+//         });
+
+//         app.get('/', function (req, res) {
+//             res.sendFile(path.join(__dirname, './public/form.html'));
+//         });
+
+//         app.listen(port, () => {
+//             console.log(`Server is running at http://localhost:${port}`);
+//         });
+
+//         console.log('All Movies:', allMovies);
+//     }
+//     catch (error) {
+//         console.error('Error retrieving movies:', error);
+//     }
+//     // finally {
+//     //     mongoose.connection.close();
+//     // }
+// })
+
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, './public/form.html'));
@@ -41,6 +73,33 @@ app.get('/', function (req, res) {
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
+
+app.post('/submit', async (req, res) => {
+    const name = req.body.name;
+    const password = req.body.password;
+    const email = req.body.email;
+    mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'Connection error:'));
+    db.once('open', async () => {
+        console.log('Connected to MongoDB!');
+        try {
+           let result = await mongoose.connection.db.collection('users').insertOne({
+                name: name,
+                email: email,
+                password: password
+            })
+            res.json(result);
+        } catch (error) {
+            console.error('Error retrieving movies:', error);
+        } finally {
+            mongoose.connection.close();
+        }
+    })
+ });
+
+
+
 
 
 
@@ -64,3 +123,44 @@ app.listen(port, () => {
 // });
 
 // // Person.insertMany([data])
+
+// try {
+//     var result = await mongoose.connection.db.collection('sessions').find().toArray()
+//     console.log(result);
+//     } catch (error) {
+//     console.error('Error retrieving movies:', error);
+//     } finally {
+//     mongoose.connection.close();
+//     }
+
+// app.get('/data', async (req, res) => {
+//     try {
+//       // Query data
+//       const result = await YourModel.find({ /* your query criteria */ });
+//       res.json(result);
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     }
+//   });
+
+
+//     // Fetch data from the server and update the DOM
+//     async function fetchData() {
+//       const response = await fetch('/data');
+//       const data = await response.json();
+
+//       const dataList = document.getElementById('dataList');
+//       dataList.innerHTML = '';
+
+//       data.forEach(item => {
+//         const listItem = document.createElement('li');
+//         listItem.textContent = item.fieldName; // Replace 'fieldName' with the actual field you want to display
+//         dataList.appendChild(listItem);
+//       });
+//     }
+
+//     // Call the fetchData function when the page loads
+//     window.onload = fetchData;
+
+
