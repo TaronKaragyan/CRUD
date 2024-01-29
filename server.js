a789701202f4d988056c025a57a5897df16fdce9
TaronKaragyan <taron.karagyan.y@tumo.org>
2024-01-29 21:27:44 +0400

    pain

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://TaronKaragyan:094690398Taron@cluster0.jctwsr0.mongodb.net/sample_mflix';
+const connectionString = 'mongodb+srv://TaronKaragyan:094690398Taron@cluster0.jctwsr0.mongodb.net/test';
 mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error:'));
@@ -14,6 +14,8 @@ db.on('error', console.error.bind(console, 'Connection error:'));
 const personSchema = new mongoose.Schema({
     name: String,
     age: Number,
+    email: String,
+    password: String,
 });
 
 
@@ -29,6 +31,7 @@ db.once('open', async () => {
         //console.log(Bloomington)
         app.post('/submit', async (req, res) => {
             const name = req.body.name;
+            const age = req.body.age;
             const password = req.body.password;
             const email = req.body.email;
             mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -37,15 +40,16 @@ db.once('open', async () => {
             db.once('open', async () => {
                 console.log('Connected to MongoDB!');
                 try {
-                    let Bloomington = await mongoose.connection.db.collection('theater').find({"location.address.city": "Bloomington"}).toArray
-                    let result = await mongoose.connection.db.collection('users').insterMany({
-                        name: name,
-                        email: email,
-                        password: password
+                    let resul1 = await Person.create({
+                        "name" : name,
+                        "age": age,
+                        "email": email,
+                        "password": password,
                     })
+                    //let result = await mongoose.connection.db.collection('people').find({'name': 'Taron1'}).toArray()
+                    //let result = await mongoose.connection.db.collection('people').insterMany(resul1)
                     res.json(result);
                 } catch (error) {
-                    console.error('Error retrieving movies:', error);
                 } finally {
                     mongoose.connection.close();
                 }
@@ -53,22 +57,41 @@ db.once('open', async () => {
          });
 
         app.get('/', function (req, res) {
+            const name = req.body.name;
             mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
             const db = mongoose.connection;
             db.on('error', console.error.bind(console, 'Connection error:'));
             db.once('open', async () => {
             try {
-                let result = await mongoose.connection.db.collection('theaters').find({'location.address.city':'Bloomington'}).toArray()
+                let result = await mongoose.connection.db.collection('people').find({'name':Taron1}).toArray()
                 res.render('../public/form.ejs', {
                     obj: result
                 });
                 } catch (error) {
-                    console.error('Error retrieving movies:', error);
                 } finally {
                     mongoose.connection.close();
                 }
             })
         });
+
+        // app.get('/name/:name', function (req, res) {
+        //     const name = req.params.name;
+        //     mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+        //     const db = mongoose.connection;
+        //     db.on('error', console.error.bind(console, 'Connection error:'));
+        //     db.once('open', async () => {
+        //     try {
+        //         let result = await mongoose.connection.db.collection('people').find({'name': Taron1}).toArray()
+        //         res.render('../public/form.ejs', {
+        //             obj: result
+        //         });
+        //         } catch (error) {
+        //         } finally {
+        //             mongoose.connection.close();
+        //         }
+        //     })
+        // });
+
 
         app.listen(port, () => {
             console.log(`Server is running at http://localhost:${port}`);
